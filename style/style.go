58d1c975ed45196ab03835fbdb87d7cfcbd0b4b2
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-04-04 19:05:07 +0200

    style: improve Scale a little bit
      - add Percent() that returns the scale as a percentage
      - round the percentage, so that 99% is rounded to 100% instead of 75%

--- style/style.go
+++ style/style.go
@@ -3,6 +3,9 @@ package style
 type Scale uint8
 
 func NewScale(percent int) Scale {
-	return Scale(percent / 25)
+	return Scale((percent + 12) / 25)
 }
 
+func (s Scale) Percent() int {
+	return int(s) * 25
+}
