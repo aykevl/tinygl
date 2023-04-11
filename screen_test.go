c38c15dbbf915f1ad358f8686a688877f535e7d7
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-04-12 00:26:51 +0200

    test: fix test after API change

--- screen_test.go
+++ screen_test.go
@@ -37,7 +37,7 @@ func TestScreen(t *testing.T) {
 
 func makeScreen[T pixel.Color](img ImageDisplay) *tinygl.Screen[T] {
 	buf := make([]T, 160*8)
-	screen := tinygl.NewScreen(img, buf)
+	screen := tinygl.NewScreen(img, buf, 47) // 120PPI ≈ 47px/cm
 	return screen
 }
 
