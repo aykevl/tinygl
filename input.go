31cd31c98d949c15660fc7f6905d67000edfaff1
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-04-16 00:50:13 +0200

    tinygl: add more touch events

--- input.go
+++ input.go
@@ -4,8 +4,10 @@ type Event uint8
 
 const (
 	NoEvent Event = iota
-	TouchTap
+	TouchStart
 	TouchMove
+	TouchEnd
+	TouchTap
 )
 
 // Set the position of the current touch point, or (-1, -1) if nothing currently
@@ -21,19 +23,26 @@ func (s *Screen[T]) SetTouchState(x, y int16) {
 			s.touchEvent = TouchTap
 			s.touchX = x
 			s.touchY = y
+			s.child.HandleEvent(TouchStart, int(x), int(y))
 		} else if s.touchEvent == TouchTap {
 			// Continuing touch event.
 			if abs(int(s.touchX)-int(x)) > int(s.ppi)/4 || abs(int(s.touchY)-int(y)) > int(s.ppi)/4 {
 				// The touch point moved more than ¼inch or 0.635mm. Treat it as
 				// a move event.
 				s.touchEvent = TouchMove
+				s.child.HandleEvent(TouchMove, int(x), int(y))
 			}
 			// TODO: long press, double tap, etc.
+		} else if s.touchEvent == TouchMove {
+			s.child.HandleEvent(TouchMove, int(x), int(y))
 		}
 	} else if s.touchEvent != NoEvent {
 		// Not touching anymore: the touch event ended.
-		// Notify the tap event.
-		s.child.HandleEvent(s.touchEvent, int(s.touchX), int(s.touchY))
+		s.child.HandleEvent(TouchEnd, -1, -1)
+		if s.touchEvent == TouchTap {
+			// Notify the tap event.
+			s.child.HandleEvent(TouchTap, int(s.touchX), int(s.touchY))
+		}
 		// ...and reset the touch state to the initial state.
 		s.touchEvent = NoEvent
 		s.touchX = x
