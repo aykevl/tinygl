c1d79a9d965bc5edfbcc622fe95ee010f0061a55
deadprogram <ron@hybridgroup.com>
2024-05-09 20:09:18 +0200

    style/*: rename type to Theme and constructor to New() to keep things consistent and cleaner
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- style/mono/objects.go
+++ style/mono/objects.go
@@ -5,17 +5,17 @@ import (
 )
 
 // Create a new text label with the default style.
-func (theme *Mono[T]) NewText(text string) *tinygl.Text[T] {
+func (theme *Theme[T]) NewText(text string) *tinygl.Text[T] {
 	return tinygl.NewText(theme.Font, theme.Foreground, theme.Background, text)
 }
 
 // NewVBox returns a new VBox with the default style.
-func (theme *Mono[T]) NewVBox(children ...tinygl.Object[T]) *tinygl.VBox[T] {
+func (theme *Theme[T]) NewVBox(children ...tinygl.Object[T]) *tinygl.VBox[T] {
 	return tinygl.NewVBox(theme.Background, children...)
 }
 
 // Create a new listbox with the given elements. The elements (and number of
 // elements) cannot be changed after creation.
-func (theme *Mono[T]) NewListBox(elements []string) *tinygl.ListBox[T] {
+func (theme *Theme[T]) NewListBox(elements []string) *tinygl.ListBox[T] {
 	return tinygl.NewListBox(theme.Font, theme.Foreground, theme.Background, theme.Tint, elements)
 }
